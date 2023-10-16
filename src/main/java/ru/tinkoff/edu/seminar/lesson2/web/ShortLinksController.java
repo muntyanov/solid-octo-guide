e9f44fb89c988bfcda3f88649a6c1491613f7396
s.muntyanov <s.muntyanov@tinkoff.ru>
2023-10-16 08:53:05 +0500

    пофиксил статус коды

--- src/main/java/ru/tinkoff/edu/seminar/lesson2/web/ShortLinksController.java
+++ src/main/java/ru/tinkoff/edu/seminar/lesson2/web/ShortLinksController.java
@@ -3,6 +3,7 @@ package ru.tinkoff.edu.seminar.lesson2.web;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,10 +29,11 @@ public class ShortLinksController {
     @Operation(summary = "Получить короткую ссылку из длинной",
             responses = {
                     @ApiResponse(responseCode = "200", description = "Возвращает объект созданной ссылки"),
+                    @ApiResponse(responseCode = "400", description = "Пустая строка не может быть урлом"),
                     @ApiResponse(responseCode = "500", description = "Не удалось сгенерировать")
             })
     @PostMapping("/link")
-    public LinkCreateResponseDto shortLinkCreate(@RequestBody String fullPath) {
+    public LinkCreateResponseDto shortLinkCreate(@RequestBody @NotEmpty String fullPath) {
         try {
             return mapper.linkToLinkCreateDto(shortLinkService.create(fullPath));
         } catch (Exception e) {
@@ -42,10 +44,11 @@ public class ShortLinksController {
     @Operation(summary = "Получить ссылку из короткой",
             responses = {
                     @ApiResponse(responseCode = "200", description = "Возвращает объект созданной ссылки"),
+                    @ApiResponse(responseCode = "400", description = "Пустая строка не может быть короткой ссылкой"),
                     @ApiResponse(responseCode = "404", description = "Ссылка не найдена")
             })
     @GetMapping("/link/{shortLink}")
-    public LinkListResponseDto shortLinkFind(@PathVariable String shortLink) {
+    public LinkListResponseDto shortLinkFind(@PathVariable @NotEmpty String shortLink) {
         var link = shortLinkService.find(shortLink);
         if (link == null) {
             throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
@@ -59,7 +62,7 @@ public class ShortLinksController {
                     @ApiResponse(responseCode = "404", description = "Ссылка не найдена")
             })
     @DeleteMapping("/link/{shortLink}")
-    public ResponseEntity shortLinkDelete(@PathVariable String shortLink) {
+    public ResponseEntity shortLinkDelete(@PathVariable @NotEmpty String shortLink) {
         if (shortLinkService.delete(shortLink))
             return ResponseEntity.noContent().build();
         return ResponseEntity.notFound().build();
